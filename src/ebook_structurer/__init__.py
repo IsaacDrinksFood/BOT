014246366c01965b7e1198a39cb19617b3619660
IsaacDrinksFood <69685881+IsaacDrinksFood@users.noreply.github.com>
2025-09-28 21:11:50 +0100

    Add research-to-ebook template generator

---
+++ src/ebook_structurer/__init__.py
@@ -0,0 +1,8 @@
+"""Utilities for transforming research notes into an e-book template."""
+
+from .builder import EbookTemplateBuilder, ResearchParser
+
+__all__ = [
+    "EbookTemplateBuilder",
+    "ResearchParser",
+]
